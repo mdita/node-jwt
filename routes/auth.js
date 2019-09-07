a531e5e4f761632280aa739a97c603adfefa664d
mihaidita <ditamihai@gmail.com>
2019-09-07 22:25:55 +0300

    generate different jwt

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,21 @@
 const router = require('express').Router();
 const jwt = require('jsonwebtoken');
-
+const uuidv1 = require('uuid/v1');
 
 router.post('/register', (req, res, next) => {
   res.send('Register route here');
 });
 
 router.post('/login', (req, res, next) => {
-  const token = jwt.sign('blabla', 'secret'); // generate token
+  const uuid = uuidv1();
+  const token = jwt.sign({
+    id: uuid,
+    text: 'blabla'
+  }, 'secret'); // generate token
 
   // instead of blabla there should be some data or something
 
+  console.log(token);
   res.header('auth-token', token); // assign to header
   res.send(token);
 });
